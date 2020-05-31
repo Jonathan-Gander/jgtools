b88452788862a627291234d5311044980dbeeeab
Jonathan Gander <j.gander@edsi-tech.com>
2020-05-31 10:35:26 +0200

    Bearing method

--- JGTools.h
+++ JGTools.h
@@ -10,6 +10,9 @@
 
 #import <UIKit/UIKit.h>
 
+// For bearing method
+#import <MapKit/MapKit.h>
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface JGTools : NSObject
@@ -25,6 +28,14 @@ NS_ASSUME_NONNULL_BEGIN
  * @return Device language in two char
  */
 + (NSString *)deviceLanguage;
+
+/**
+ * Bearing between two coordinates
+ * @param coordinate1 First coordinate
+ * @param coordinate2 Second coordinate
+ * @return Bearing in degrees
+ */
++ (double)bearingFromCoordinate:(CLLocationCoordinate2D)coordinate1 toCoordinate:(CLLocationCoordinate2D)coordinate2;
 
 @end
 
--- JGTools.m
+++ JGTools.m
@@ -8,6 +8,10 @@
 
 #import "JGTools.h"
 
+// For bearing method
+#define degreesToRadians(x) (M_PI * x / 180.0)
+#define radiandsToDegrees(x) (x * 180.0 / M_PI)
+
 @implementation JGTools
 
 #pragma mark - Color
@@ -37,6 +41,32 @@
     }
 
     return language;
+}
+
+#pragma mark - Bearing
++ (double)bearingFromCoordinate:(CLLocationCoordinate2D)coordinate1 toCoordinate:(CLLocationCoordinate2D)coordinate2 {
+    
+    // Sources :
+    // http://www.movable-type.co.uk/scripts/latlong.html
+    // http://stackoverflow.com/questions/8123049/calculate-bearing-between-two-locations-lat-long
+    // http://software.intel.com/en-us/blogs/2012/11/30/calculating-a-bearing-between-points-in-location-aware-apps
+    // http://stackoverflow.com/questions/3809337/calculating-bearing-between-two-cllocationcoordinate2ds
+    
+    double lat1 = degreesToRadians(coordinate1.latitude);
+    double lng1 = degreesToRadians(coordinate1.longitude);
+    double lat2 = degreesToRadians(coordinate2.latitude);
+    double lng2 = degreesToRadians(coordinate2.longitude);
+    
+    double dLon = (lng2-lng1);
+    double y = sin(dLon) * cos(lat2);
+    double x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon);
+    double bearing = radiandsToDegrees(atan2(y, x));
+    
+    if (bearing < 0) {
+        bearing += 360;
+    }
+    
+    return bearing;
 }
 
 @end
