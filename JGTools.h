8503fe7cc1689d4dfb469196edacde9dea2ea36d
Jonathan Gander <j.gander@edsi-tech.com>
2020-05-31 20:00:52 +0200

    Refacto : method comment details are in .m file.

--- JGTools.h
+++ JGTools.h
@@ -17,33 +17,20 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface JGTools : NSObject
 
-/**
-* Get a UIColor from a hex string
-* @param str String representing hex for color. It can start with or without '#'.
-* @return UIColor for hex string
-*/
+#pragma mark - Color
 + (UIColor *)colorWithHexString:(NSString *)str;
 
-/**
- * @return Device language in two char
- */
-+ (NSString *)deviceLanguage;
-
-/**
- * Check if an URL string is an image. It only check for extension.
- * Note : A better solution would be to make a HEAD request and read Content-Type header.
- * @param string URL string
- * @return YES if URL is an image, NO otherwise
- */
+#pragma mark - Image
 + (BOOL)urlStringIsImage:(NSString *)string;
 
-/**
- * Bearing between two coordinates
- * @param coordinate1 First coordinate
- * @param coordinate2 Second coordinate
- * @return Bearing in degrees
- */
+#pragma mark - Date and time
++ (NSTimeInterval)currentTimestamp;
+
+#pragma mark - Location and bearing
 + (double)bearingFromCoordinate:(CLLocationCoordinate2D)coordinate1 toCoordinate:(CLLocationCoordinate2D)coordinate2;
+
+#pragma mark - Device
++ (NSString *)deviceLanguage;
 
 @end
 
--- JGTools.m
+++ JGTools.m
@@ -15,7 +15,11 @@
 @implementation JGTools
 
 #pragma mark - Color
-
+/**
+ * Get a UIColor from a hex string
+ * @param str String representing hex for color. It can start with or without '#'.
+ * @return UIColor for hex string
+ */
 + (UIColor *)colorWithHexString:(NSString *)str {
     if (![[str substringToIndex:1] isEqualToString:@"#"])
         str = [NSString stringWithFormat:@"#%@",str];
@@ -32,18 +36,13 @@
     return [UIColor colorWithRed:(float)r/255.0f green:(float)g/255.0f blue:(float)b/255.0f alpha:1];
 }
 
-#pragma mark - Device language
-+ (NSString *)deviceLanguage {
-    NSString *language = [[NSLocale preferredLanguages] objectAtIndex:0];
-    NSRange startRange = [language rangeOfString:@"-"];
-    if (startRange.location != NSNotFound) {
-        language = [language substringToIndex:startRange.location];
-    }
-
-    return language;
-}
-
 #pragma mark - Image
+/**
+ * Check if an URL string is an image. It only check for extension.
+ * Note : A better solution would be to make a HEAD request and read Content-Type header.
+ * @param string URL string
+ * @return YES if URL is an image, NO otherwise
+ */
 + (BOOL)urlStringIsImage:(NSString *)string {
     NSArray *imageExtensions = @[@"jpg", @"jpeg", @"jpe", @"jif", @"jfif", @"jfi", @"png", @"gif", @"webp", @"tiff", @"tif", @"bmp", @"jp2", @"j2k", @"jpf", @"jpx", @"jpm", @"mj2"];
 
@@ -55,7 +54,23 @@
     NSString *extension = [[url pathExtension] lowercaseString];
     return [imageExtensions containsObject:extension];
 }
-#pragma mark - Bearing
+
+#pragma mark - Date and time
+/**
+ * @return Current timestamp in seconds since 1970
+ */
++ (NSTimeInterval)currentTimestamp {
+    NSDate *now = [NSDate date];
+    return [now timeIntervalSince1970];
+}
+
+#pragma mark - Location and bearing
+/**
+ * Bearing between two coordinates
+ * @param coordinate1 First coordinate
+ * @param coordinate2 Second coordinate
+ * @return Bearing in degrees
+ */
 + (double)bearingFromCoordinate:(CLLocationCoordinate2D)coordinate1 toCoordinate:(CLLocationCoordinate2D)coordinate2 {
     
     // Sources :
@@ -79,6 +94,20 @@
     }
     
     return bearing;
+}
+
+#pragma mark - Device
+/**
+ * @return Device language as two chars
+ */
++ (NSString *)deviceLanguage {
+    NSString *language = [[NSLocale preferredLanguages] objectAtIndex:0];
+    NSRange startRange = [language rangeOfString:@"-"];
+    if (startRange.location != NSNotFound) {
+        language = [language substringToIndex:startRange.location];
+    }
+
+    return language;
 }
 
 @end
